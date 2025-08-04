57e58840ab2defdb15f89f8f9aea5eca65d3a517
Joao <joaopedrosbt00@gmail.com>
2025-08-04 09:53:02 -0300

    Bootstrap + home page adicionado

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from materias import views as materias_views
+from livros import views as livros_views
 
 urlpatterns = [
-    path("livros/", include("livros.urls")),
     path('admin/', admin.site.urls),
+    path('', livros_views.home, name='home'),
+    #livros
+    path("livros/", include("livros.urls")),
     #materias
     path('materia/', materias_views.index, name='index'),
     path('materias/adicionar/', materias_views.add, name='add'),
--- livros/views.py
+++ livros/views.py
@@ -1,20 +1,9 @@
-from django.http import HttpResponse
-from .models import livros
+from django.shortcuts import render
 
-
-def index(request):
-    latest_livros_list = livros.objects.order_by('-ano')[:5]
-    output = ", ".join([q.titulo for q in latest_livros_list])
-    return HttpResponse(output)
-
-def detail(request, livros_id):
-    return HttpResponse("You're looking at question %s." % livros_id)
-
-
-def results(request, livros_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % livros_id)
-
-
-def vote(request, livros_id):
-    return HttpResponse("You're voting on question %s." % livros_id)
+def criar_livro(request):
+    if request.method == 'GET':
+        return render(request, 'livros/criar_livro.html')
+    
+def home(request):
+    if request.method == 'GET':
+        return render(request, 'livros/index.html')
